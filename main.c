3dea7626a976473e37519b5afc5d598a475ecf59
SwipSwup <david.veigel2002@gmail.com>
2023-09-15 16:17:31 +0200

    Finish "Logische Operatoren"

--- main.c
+++ main.c
@@ -6,8 +6,9 @@ int main() {
     scanf("%d", &x);
     scanf("%d", &y);
 
-    printf("%d\n",  x < y);
-    printf("%d\n",  x > y);
-    printf("%d\n",  x == y);
+    printf("%d\n",  x > 0 && x < y);
+    printf("%d\n",  x == y || y < 0);
+    printf("%d\n",  (!x || !y) && x != y);
+
     return 0;
 }
