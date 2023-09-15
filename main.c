8783761f143d7fb93b1ae0a135c9f1b8dbeebfa3
SwipSwup <david.veigel2002@gmail.com>
2023-09-15 16:08:14 +0200

    Finish "Arithmetische Operatoren"

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int x, y;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    printf("%d\n", x = y + 5);
+    printf("%d\n", y *= 3);
+    printf("%d\n", x = y / 2);
+    printf("%d\n", x % y);
     return 0;
 }
