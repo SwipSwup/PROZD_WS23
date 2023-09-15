97ebc1674dbb90df86a4571e9b47e3101d12cff0
SwipSwup <david.veigel2002@gmail.com>
2023-09-15 16:11:07 +0200

    Finish "Relationale Operatoren"

--- main.c
+++ main.c
@@ -6,9 +6,8 @@ int main() {
     scanf("%d", &x);
     scanf("%d", &y);
 
-    printf("%d\n", x = y + 5);
-    printf("%d\n", y *= 3);
-    printf("%d\n", x = y / 2);
-    printf("%d\n", x % y);
+    printf("%d\n",  x < y);
+    printf("%d\n",  x > y);
+    printf("%d\n",  x == y);
     return 0;
 }
