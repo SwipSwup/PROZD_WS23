5d27a47fc503f7e24682833bde23025c9fddf417
SwipSwup <david.veigel2002@gmail.com>
2023-09-18 13:11:54 +0200

    Finish UE5

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main() {
     char time24[6];
-    scanf("%5s", &time24);
+    scanf("%5s", &time24[0]);
 
     int hour = (time24[0] - 48) * 10 + time24[1] - 48;
+    printf("24h time: 12h time: ");
 
-    if(hour <= 12) {
-        printf("%s AM", time24);
+    if (!hour) {
+        printf("12:%c%c AM", time24[3], time24[4]);
         return 0;
     }
 
-    printf("%d:%c%c PM", );
+    if(hour < 13) {
+        printf("%s %s", time24, hour < 12 ? "AM" : "PM");
+        return 0;
+    }
 
+    printf("%2d:%c%c %s", hour - 12, time24[3], time24[4], hour == 24 ? "AM" : "PM");
     return 0;
 }
 
