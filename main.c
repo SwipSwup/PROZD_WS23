b87b8b954de61d97ee1e7d8cc3c62ca22a35ad79
SwipSwup <david.veigel2002@gmail.com>
2023-09-18 12:46:33 +0200

    Add AM functionality

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char time24[6];
+    scanf("%5s", &time24);
+
+    int hour = (time24[0] - 48) * 10 + time24[1] - 48;
+
+    if(hour <= 12) {
+        printf("%s AM", time24);
+        return 0;
+    }
+
+    printf("%d:%c%c PM", );
+
     return 0;
 }
+
+
