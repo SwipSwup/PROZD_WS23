6f59a7ac3d7c94a6efcdfc681fe127352fc94874
SwipSwup <david.veigel2002@gmail.com>
2023-09-18 13:39:52 +0200

    Finish UE6

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int n[5];
+
+    scanf("%d %d %d %d %d", &n[0], &n[1], &n[2], &n[3], &n[4]);
+    int min = n[0], max = n[0];
+
+    for (int i = 1; i < 5; ++i) {
+        if (min > n[i])
+            min = n[i];
+        else if (max < n[i])
+            max = n[i];
+    }
+
+    printf(": minimum sum: %d\n", n[1] + n[2] + n[3] + min);
+    printf("maximum sum: %d", n[1] + n[2] + n[3] + max);
+
     return 0;
 }
