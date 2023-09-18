86460aae0829b790d3cdbd6409790ddf9a1e908f
SwipSwup <david.veigel2002@gmail.com>
2023-09-18 12:22:24 +0200

    Finish UE4

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "math.h"
+
+int convert(int in, int base) {
+    return in % 10 + (in / 10) % 10 * base + (in / 100) % 10 * pow(base, 2);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int okt;
+    int zs;
+
+    scanf("%d", &okt);
+
+    printf(": dec.: %d\n", zs = convert(okt, 8));
+    printf("ZS: %d\n", convert(zs, 1));
+
     return 0;
 }
+
+
