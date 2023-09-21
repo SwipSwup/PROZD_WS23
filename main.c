3f05ea530b74eb7a8f91d856bd37d2bc8e932285
SwipSwup <david.veigel2002@gmail.com>
2023-09-21 10:19:47 +0200

    Finish "for-Schleifen Part 2"

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 20; i++) {
+    int i;
+    scanf("%d", &i);
+
+    for (; i <= 20; i++) {
         printf("%d ", i);
     }
     return 0;
