f5b4b89383cceb07c7c21b8e790ed8a44ed53e47
SwipSwup <david.veigel2002@gmail.com>
2023-09-22 12:33:34 +0200

    Start implementing algorithm

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h.h>
+
+int getPrimes(int n);
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
+}
+
+int getPrimes(int n) {
+    int k = (n - 1) / n;
+    int indecies[k + 1];
+
+    
+
+    for (int i = 1; i < sqrt(k); ++i) {
+        int j = i;
+        while (i + j + 2 * i * j){
+            indecies[i + j + 2 * i * j] = 0;
+        }
+    }
+
 }
