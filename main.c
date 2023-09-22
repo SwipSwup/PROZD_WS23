2c4f8e316b5a732aa843ae62548b26050b9629d0
SwipSwup <david.veigel2002@gmail.com>
2023-09-22 12:47:26 +0200

    End of lesson push

--- main.c
+++ main.c
@@ -12,14 +12,23 @@ int main() {
 int getPrimes(int n) {
     int k = (n - 1) / n;
     int indecies[k + 1];
+    int nonePrimeCount;
 
-    
+    for (int i = 0; i < k + 1; ++i)
+        indecies[i] = 1;
 
     for (int i = 1; i < sqrt(k); ++i) {
         int j = i;
         while (i + j + 2 * i * j){
             indecies[i + j + 2 * i * j] = 0;
+            nonePrimeCount++;
+            j++;
         }
+    }
+
+    int primes[nonePrimeCount];
+    for (int i = 0; i < k + 1; ++i) {
+        if()
     }
 
 }
