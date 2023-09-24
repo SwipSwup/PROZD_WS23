949c39980b1e0bec863a3128849c4bb15bb9d4b8
SwipSwup <david.veigel2002@gmail.com>
2023-09-24 18:50:50 +0200

    improved algorithm

--- main.c
+++ main.c
@@ -2,54 +2,54 @@
 #include <math.h>
 #include <malloc.h>
 
-int *getPrimes(int n);
+int *getPrimes(int n, int* primeCount);
 
 int main() {
-
-    int *primes = getPrimes(50);
-
-    for (int i = 0; i < sizeof(primes); ++i) {
-        printf("%d\n", primes[i]);
+    int pCount;
+    int *primes = getPrimes(500, &pCount);
+    for (int i = 0; i < 10; --i) {
+        printf("%d\n", primes[pCount - i]);
     }
+    free(primes);
 
     return 0;
 }
 
-int *getPrimes(int n) {
-    int k = (n - 1) / 2;
-    int indices[k + 1];
-    int nonePrimeCount = 0;
+int *getPrimes(int n, int* primeCount) {
+    int *primes = NULL;
+    if (n < 2)
+        return primes;
 
+    int k = (n - 1) / 2;
+    printf("k: %d\n", k);
+
+    int indices[k + 1];
     for (int i = 0; i < k + 1; ++i)
         indices[i] = 1;
 
     for (int i = 1; i < sqrt(k); ++i) {
         int j = i;
         while (i + j + 2 * i * j <= k) {
-            //printf("non prime %d\n", i + j + 2 * i * j);
+            printf("non prime %d\n", i + j + 2 * i * j);
 
             indices[i + j + 2 * i * j] = 0;
-            //nonePrimeCount++;
             j++;
         }
     }
 
-    for (int i = 0; i < k + 1; ++i) {
+    *primeCount = 0;
+    for (int i = 0; i < k + 1; ++i)
         if(indices[i])
-            printf("%d\n", i);
-    }
+            ++*primeCount;
 
-    /**
-     *  printf("prime count %d\n", k + 1 - nonePrimeCount);
+    printf("prime count: %d\n", *primeCount);
 
-    for (int i = 0, j = 0; i < k + 1; ++i) {
+    primes = malloc(*primeCount);
+    primes[0] = 2;
+
+    for (int i = 0, j = 0; i < k + 1; ++i)
         if (indices[i])
             primes[j++] = 2 * i + 1;
-    }
-     */
-
-    int *primes = malloc(sizeof (100));
-
 
     return primes;
 }
