e6e511e6d264a4c35674a4366f2537212d7153f2
SwipSwup <david.veigel2002@gmail.com>
2023-09-25 10:37:10 +0200

    Angabe nicht verstanden

--- main.c
+++ main.c
@@ -6,9 +6,9 @@ int *getPrimes(int n, int* primeCount);
 
 int main() {
     int pCount;
-    int *primes = getPrimes(500, &pCount);
-    for (int i = 0; i < 10; --i) {
-        printf("%d\n", primes[pCount - i]);
+    int *primes = getPrimes(1010, &pCount);
+    for (int i = 0; i < (pCount < 10 ? pCount : 10); ++i) {
+        printf("%d\n", primes[pCount - i - 1]);
     }
     free(primes);
 
@@ -21,16 +21,17 @@ int *getPrimes(int n, int* primeCount) {
         return primes;
 
     int k = (n - 1) / 2;
-    printf("k: %d\n", k);
+    //printf("k: %d\n", k);
 
     int indices[k + 1];
     for (int i = 0; i < k + 1; ++i)
         indices[i] = 1;
 
-    for (int i = 1; i < sqrt(k); ++i) {
+    //for (int i = 1; i < sqrt(k); ++i) { Die Abgabe kann sqrt nicht compilieren
+    for (int i = 1; i < k - 1; ++i) {
         int j = i;
         while (i + j + 2 * i * j <= k) {
-            printf("non prime %d\n", i + j + 2 * i * j);
+            //printf("non prime %d\n", i + j + 2 * i * j);
 
             indices[i + j + 2 * i * j] = 0;
             j++;
@@ -42,14 +43,16 @@ int *getPrimes(int n, int* primeCount) {
         if(indices[i])
             ++*primeCount;
 
-    printf("prime count: %d\n", *primeCount);
+    //printf("prime count: %d\n", *primeCount);
 
     primes = malloc(*primeCount);
-    primes[0] = 2;
 
-    for (int i = 0, j = 0; i < k + 1; ++i)
-        if (indices[i])
+    for (int i = 0, j = 0; i < k + 1; ++i) {
+        if (indices[i]) {
             primes[j++] = 2 * i + 1;
+        }
+    }
+    primes[0] = 2;
 
     return primes;
 }
