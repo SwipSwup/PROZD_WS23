5f727b5967cd0968379260697a63b65cb3df38ae
SwipSwup <david.veigel2002@gmail.com>
2023-09-26 17:32:34 +0200

    Finish UE 8

--- main.c
+++ main.c
@@ -3,19 +3,19 @@
 int main() {
     int calenderLength, startingDayIndex;
 
-    printf("number of days in month:");
+    printf("number of days in month: ");
     scanf("%d", &calenderLength);
-    printf("\nstarting day of week (1=Monday, 7=Sunday):");
+    printf("starting day of week (1=Monday, 7=Sunday):");
     scanf("%d", &startingDayIndex);
 
-    for (int i = 0; i < calenderLength + startingDayIndex; ++i) {
-        if(!(i % 7))
+    for (int i = 1; i < calenderLength + startingDayIndex; ++i) {
+        if(!((i - 1) % 7))
             printf("\n");
 
-        if(i < startingDayIndex - 1)
+        if(i < startingDayIndex)
             printf("   ");
         else
-            printf("%2d ", i + 1 - startingDayIndex - 1);
+            printf("%2d ", i - startingDayIndex + 1);
     }
 
     return 0;
