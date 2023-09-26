32bc5cac9a9dce94b47f0e55e8394238b7215514
SwipSwup <david.veigel2002@gmail.com>
2023-09-26 12:20:24 +0200

    Began implementing solution

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int calenderLength, startingDayIndex;
+
+    printf("number of days in month:");
+    scanf("%d", &calenderLength);
+    printf("\nstarting day of week (1=Monday, 7=Sunday):");
+    scanf("%d", &startingDayIndex);
+
+    for (int i = 0; i < calenderLength + startingDayIndex; ++i) {
+        if(!(i % 7))
+            printf("\n");
+
+        if(i < startingDayIndex - 1)
+            printf("   ");
+        else
+            printf("%2d ", i - startingDayIndex);
+    }
+
     return 0;
 }
