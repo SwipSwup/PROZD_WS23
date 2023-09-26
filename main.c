c96bd510d69cd9cd074dea13999a398c3cd4f98f
SwipSwup <david.veigel2002@gmail.com>
2023-09-26 12:48:49 +0200

    ugly mess

--- main.c
+++ main.c
@@ -1,16 +1,46 @@
 #include <stdio.h>
 
 int main() {
+    int input;
     int grades[3];
     int gSum = 0;
 
-    for (int i = 0; i < 3; ++i) {
-        printf("Noten: ");
-        scanf("%1d", &grades[i]);
+    printf("Noten: ");
+    scanf("%d", &input);
+
+    if (input / 1000) {
+        printf("Erwarte genau 3 Noten!");
+        return 0;
     }
 
     for (int i = 0; i < 3; ++i) {
-        if(grades[i] == 5) {
+        grades[i] = input % 10;
+        input /= 10;
+
+        if(input == 0) {
+            printf("Erwarte genau 3 Noten!");
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        if (grades[i] < 1 || grades[i] > 5) {
+
+            switch (i) {
+                case 0:
+                    printf("Erste Note muss zwischen 1 und 5 liegen!");
+                    return 0;
+                case 1:
+                    printf("Zweite Note muss zwischen 1 und 5 liegen!");
+                    return 0;
+                case 2:
+                    printf("Dritte Note muss zwischen 1 und 5 liegen!");
+                    return 0;
+            }
+
+        }
+
+        if (grades[i] == 5) {
             printf("Nicht bestanden!");
             return 0;
         }
@@ -18,7 +48,7 @@ int main() {
         gSum += grades[i];
     }
 
-    switch(gSum) {
+    switch (gSum) {
         case 3:
             printf("Ausgezeichneter Erfolg!");
             break;
