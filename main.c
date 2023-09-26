338e0d3881e918a6ce7a07e9bd09dfbb3b2eb499
SwipSwup <david.veigel2002@gmail.com>
2023-09-26 18:15:16 +0200

    Finish UE9

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include <float.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int cnt = 0;
+    double min = DBL_MAX, max = DBL_MIN, sum = 0, n;
+
+    do {
+        printf(": ");
+        scanf("%lf", &n);
+
+        if (n > 0) {
+            cnt++;
+            sum += n;
+
+            if (n > max) {
+                max = n;
+            }
+            if (n < min) {
+                min = n;
+            }
+        }
+    } while (n > 0);
+
+    if (cnt == 0) {
+        printf("no number entered");
+        return 0;
+    }
+
+    printf("cnt: %d\n", cnt);
+    printf("min: %.2lf\n", min);
+    printf("max: %.2lf\n", max);
+    printf("avg: %.2lf", sum / cnt);
+
     return 0;
 }
