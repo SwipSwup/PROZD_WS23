9cfb61631d4ec95376cbeaf654cb550de363b697
SwipSwup <david.veigel2002@gmail.com>
2023-09-26 11:58:15 +0200

    Without length check

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int grades[3];
+    int gSum = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        printf("Noten: ");
+        scanf("%1d", &grades[i]);
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        if(grades[i] == 5) {
+            printf("Nicht bestanden!");
+            return 0;
+        }
+
+        gSum += grades[i];
+    }
+
+    switch(gSum) {
+        case 3:
+            printf("Ausgezeichneter Erfolg!");
+            break;
+        case 4:
+            printf("Guter Erfolg!");
+            break;
+        default:
+            printf("Bestanden.");
+    }
+
     return 0;
 }
