a08f6959087484ce0053740214002f084819198d
SwipSwup <david.veigel2002@gmail.com>
2023-09-27 12:17:07 +0200

    Finish UE10

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int n, i = 1, firstPos = 1, startPos = 1;
+
+    do {
+        printf(": ");
+        scanf("%d", &n);
+
+        i++;
+        if (n != 0 && n <= startPos) {
+            startPos++;
+            printf("position #1: %d\n", startPos);
+        }
+
+        if (n == 1) {
+            firstPos = i;
+            printf("new leader: #%d\n", i);
+        }
+    } while (n != 0);
+
+    printf("final position #1: %d\n", startPos);
+    printf("winner: #%d", firstPos);
     return 0;
 }
