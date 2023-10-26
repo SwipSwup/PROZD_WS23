6f208d25589dd83293db4e24c5a32567cdac0abd
SwipSwup <david.veigel2002@gmail.com>
2023-10-26 18:12:09 +0200

    Finish Verkettete Listen 1

--- main.c
+++ main.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <malloc.h>
+
+struct node {
+    int number;
+    struct node *head;
+} typedef node;
+
+node *addBefore(int number, node *head) {
+    node *_node = (node *) malloc(sizeof(node));
+    _node->head = head;
+    _node->number = number;
+
+    return _node;
+}
+
+void freeFromNode(node *node) {
+    if (node == NULL)
+        return;
+
+    freeFromNode(node->head);
+    free(node);
+}
+
+void printFromNode(node *node) {
+    if (node == NULL)
+        return;
+    printf("%d ", node->number);
+    printFromNode(node->head);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int number = 0;
+    node *node = NULL;
+
+    while (1) {
+        scanf(" %d", &number);
+        if (!number) {
+            break;
+        }
+        node = addBefore(number, node);
+    }
+
+    printFromNode(node);
+    freeFromNode(node);
+
     return 0;
 }
