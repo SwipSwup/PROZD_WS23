ce31f11d70c48e21bdc48612e82d67fe01c42823
SwipSwup <david.veigel2002@gmail.com>
2023-10-27 09:17:24 +0200

    Begann on Verkettete Listen 2

--- main.c
+++ main.c
@@ -3,18 +3,52 @@
 
 struct node {
     int number;
-    struct node *head;
+    struct node* head;
 } typedef node;
 
-node *addBefore(int number, node *head) {
-    node *_node = (node *) malloc(sizeof(node));
-    _node->head = head;
-    _node->number = number;
+node* createNode(node* head) {
+    node* nodePtr = (node*) malloc(sizeof(node));
+    nodePtr->head = head;
 
-    return _node;
+    return nodePtr;
 }
 
-void freeFromNode(node *node) {
+node* addBefore(int number, node* head) {
+    node* nodePtr = createNode(head);
+    nodePtr->number = number;
+
+    return nodePtr;
+}
+
+int size(node* node) {
+    int i = 0;
+    for (; node != NULL; ++i, node = node->head);
+
+    return i;
+}
+
+node* get(int index, node* node) {
+    for (int i = 0; i < index; ++i, node = node->head);
+    return node;
+}
+
+node* deleteAt(int index, node* node) {
+    if (index > size(node) - 1) {
+        return node;
+    }
+
+    struct node* head = get(index, node)->head;
+
+    if (index == 0) {
+        free(get(index, node));
+        return head;
+    }
+
+    get(index - 1, node)->head = head;
+    return node;
+}
+
+void freeFromNode(node* node) {
     if (node == NULL)
         return;
 
@@ -22,7 +56,7 @@ void freeFromNode(node *node) {
     free(node);
 }
 
-void printFromNode(node *node) {
+void printFromNode(node* node) {
     if (node == NULL)
         return;
     printf("%d ", node->number);
@@ -30,18 +64,27 @@ void printFromNode(node *node) {
 }
 
 int main() {
-    int number = 0;
-    node *node = NULL;
+    int i = 0;
+    node* node = NULL;
 
     while (1) {
-        scanf(" %d", &number);
-        if (!number) {
+        scanf(" %d", &i);
+        if (!i) {
             break;
         }
-        node = addBefore(number, node);
+        node = addBefore(i, node);
     }
-
     printFromNode(node);
+
+    while (1) {
+        scanf(" %d", &i);
+        if (i < 0) {
+            break;
+        }
+
+        deleteAt(i, node);
+        printFromNode(node);
+    }
     freeFromNode(node);
 
     return 0;
