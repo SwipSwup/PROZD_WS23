6ac6991c5978c056bf37285c3bd7105b64463606
SwipSwup <david.veigel2002@gmail.com>
2023-11-03 17:01:48 +0100

    argh

--- main.c
+++ main.c
@@ -2,83 +2,30 @@
 #include <malloc.h>
 #include <string.h>
 
-struct node
+
+
+int countWord(char* text, char* word)
 {
-    char* word;
-    int count;
-    struct node* head;
-} typedef node;
+    char tmp[strlen(text) + 1];
+    strcpy(tmp, text);
+    char* token = strtok(tmp, " ");
+    int count = 0;
 
-node* createNode(char* word, node* head)
-{
-    node* newNode = (node*) malloc(sizeof(node));
-    newNode->head = head == NULL ? NULL : head;
-    newNode->word = word;
-    newNode->count = 1;
-    return newNode;
-}
-
-int size(node* map)
-{
-    int i = 0;
-    for (; map->head != NULL; i++, map = map->head);
-
-    return i;
-}
-
-
-node* getNode(int index, node* map)
-{
-    node* node = map->head;
-
-    if (index > size(map) || index < 0)
+    while (token != NULL)
     {
-        return NULL;
+        //printf("token: %s\n", token);
+        //printf("word: %s\n", word);
+        //printf("string: %s\n", text);
+        if (strstr(token, word) != NULL)
+        {
+            count++;
+        }
+
+        token = strtok(NULL, " ");
     }
 
-    if (!index)
-    {
-        return node;
-    }
-
-    for (int i = 0; i < index - 1; ++i)
-    {
-        node = node->head;
-    }
-    return node;
+    return count;
 }
-
-int nodeExists(char* word, node* map)
-{
-    if (!strcmp(word, map->word))
-    {
-        return 1;
-    }
-
-    if (map->head == NULL)
-    {
-        return 0;
-    }
-
-    return nodeExists(word, map->head);
-}
-
-void updateNode()
-{
-
-}
-
-void addNode(node* newNode, node* map)
-{
-    if (map->head == NULL)
-    {
-        map->head = newNode;
-        return;
-    }
-
-    addNode(newNode, map->head);
-}
-
 
 char** scanWords()
 {
@@ -103,49 +50,28 @@ char** scanWords()
     return words;
 }
 
-int countWord(char* text, char* word)
+void freeAll(char** strngs)
 {
-    node* map = NULL;
-
-    char* nextWord = NULL;
-    for (int i = 0; text[i] != '\0'; ++i)
+    for (int i = 0; strcmp(strngs[i], "") != 0; ++i)
     {
-        if (text[i] < 'a' || text[i] > 'z' || text[i] < 'A' || text[i] > 'Z')
-        {
-            if (nextWord == NULL)
-                continue;
-
-            if (nodeExists(word, map))
-            {
-                updateNode()
-            }
-            else
-            {
-                node* newNode = createNode(nextWord, map);
-
-                addNode()
-            }
-
-            nextWord == NULL;
-        }
+        free(strngs[i]);
     }
+    free(strngs);
 }
-
 
 int main()
 {
-    char text[1001];
-
     printf("text: ");
-    scanf("%1000s", &text[0]);
+    char text[1001];
+    fgets(text, sizeof(text), stdin);
 
-    printf("\nwords: ");
+    printf("words: ");
     char** words = scanWords();
     for (int i = 0; strcmp(words[i], "") != 0; ++i)
     {
-        printf("%s \n", words[i]);
-
+        printf("\n%s: %d", words[i], countWord(text, words[i]));
     }
 
+    freeAll(words);
     return 0;
 }
