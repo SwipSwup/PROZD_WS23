fe68b8a7a803e2b5ff81e4886d95bfd685d70bc9
SwipSwup <david.veigel2002@gmail.com>
2023-11-05 18:44:53 +0100

    gesundheit

--- main.c
+++ main.c
@@ -9,46 +9,77 @@ struct node
     struct node* head;
 } typedef node;
 
-node* createNode(int id, char* name)
+node* createNode(char* name)
 {
-    node* newNode = (node*) malloc(sizeof(node*));
-    newNode->id = id;
-    newNode->name = name;
+    node* newNode = (node*) malloc(sizeof(node*) );
+    newNode->id = 0;
+    newNode->name = (char*) malloc(strlen(name) + 1);
+    strcpy(newNode->name, name);
     newNode->head = NULL;
 
     return newNode;
 }
 
-node* addLast(node* newNode, node* list)
+void addLast(node* newNode, node* list)
+{
+    if (list->head == NULL)
+    {
+        newNode->id = list->id + 1;
+        list->head = newNode;
+        return;
+    }
+
+    addLast(newNode, list->head);
+}
+
+void printList(node* list)
 {
     if (list == NULL)
     {
-        return newNode;
+        return;
     }
 
-    if (list->head)
-    {
-        list->head = newNode;
-        return list;
+    printf("%d: %s\n", list->id, list->name);
+    printList(list->head);
+}
+
+void freeAll(node* list) {
+    if(list == NULL) {
+        return;
     }
 
-    return addLast(newNode, list->head);
+    free(list->name);
+    freeAll(list->head);
+    free(list);
 }
 
 int main()
 {
     char name[51];
+    node* list = NULL;
 
     while (1)
     {
         scanf("%50s", &name[0]);
 
-
         if (!strcmp(name, "exit"))
         {
             break;
         }
+
+        if (list == NULL)
+        {
+            list = createNode(name);
+        }
+        else
+        {
+            addLast(createNode(name), list);
+        }
+
+        printList(list);
+        printf("\n");
     }
 
+    freeAll(list);
     return 0;
 }
