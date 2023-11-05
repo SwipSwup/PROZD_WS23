ae11587dd2ca36f39732254065ede0cc5ce687c5
SwipSwup <david.veigel2002@gmail.com>
2023-11-05 18:03:08 +0100

    uga

--- main.c
+++ main.c
@@ -1,130 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 #include <malloc.h>
 
 struct node
 {
-    int number;
+    unsigned int id;
+    char* name;
     struct node* head;
 } typedef node;
 
-node* createNode(node* head)
+node* createNode(int id, char* name)
 {
-    node* nodePtr = (node*) malloc(sizeof(node));
-    nodePtr->head = head;
+    node* newNode = (node*) malloc(sizeof(node*));
+    newNode->id = id;
+    newNode->name = name;
+    newNode->head = NULL;
 
-    return nodePtr;
+    return newNode;
 }
 
-node* get(int index, node* node)
+node* addLast(node* newNode, node* list)
 {
-    for (int i = 0; i < index; ++i, node = node->head);
-    return node;
-}
-
-node* addBefore(int number, node* head)
-{
-    node* nodePtr = createNode(head);
-    nodePtr->number = number;
-
-    return nodePtr;
-}
-
-int size(node* node)
-{
-    int i = 0;
-    for (; node != NULL; ++i, node = node->head);
-
-    return i;
-}
-
-node* addAfter(int number, node* node)
-{
-    struct node* nodePtr = createNode(NULL);
-    nodePtr->number = number;
-
-    if (node != NULL)
+    if (list == NULL)
     {
-        get(size(node) - 1, node)->head = nodePtr;
-        return node;
+        return newNode;
     }
 
-    return nodePtr;
-}
-
-
-node* deleteAt(int index, node* node)
-{
-    if (index > size(node) - 1)
+    if (list->head)
     {
-        return node;
+        list->head = newNode;
+        return list;
     }
 
-
-    if (index == 0)
-    {
-        struct node* head = get(index, node)->head;
-        free(node);
-        return head;
-    }
-
-    struct node* nodeAtIndex = get(index, node);
-
-    get(index - 1, node)->head = nodeAtIndex->head;
-    free(nodeAtIndex);
-    return node;
-}
-
-void freeFromNode(node* node)
-{
-    if (node == NULL)
-        return;
-
-    freeFromNode(node->head);
-    free(node);
-}
-
-void printFromNode(node* node)
-{
-    if (node == NULL)
-    {
-        return;
-    }
-
-    printf("%d ", node->number);
-    printFromNode(node->head);
+    return addLast(newNode, list->head);
 }
 
 int main()
 {
-    int i = 0;
-    node* node = NULL;
+    char name[51];
 
     while (1)
     {
-        scanf(" %d", &i);
-        if (!i)
+        scanf("%50s", &name[0]);
+
+
+        if (!strcmp(name, "exit"))
         {
             break;
         }
-        node = addAfter(i, node);
     }
-    printFromNode(node);
-    printf("\n");
-
-    while (1)
-    {
-        scanf(" %d", &i);
-        if (i < 0)
-        {
-            break;
-        }
-
-        node = deleteAt(i, node);
-        printFromNode(node);
-        printf("\n");
-
-    }
-    freeFromNode(node);
 
     return 0;
 }
