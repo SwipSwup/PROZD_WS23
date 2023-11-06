6d8dd795e452607a4e25d929cb2fe3a9fa2c34e0
SwipSwup <david.veigel2002@gmail.com>
2023-11-06 20:57:06 +0100

    oger

--- main.c
+++ main.c
@@ -33,23 +33,38 @@ appointment createAppointment()
     appointment newAppointment;
     char tempType;
 
-    printf("\nGib einen Typen ein: Freunde (f), Geschaeftlich (b), Pause (p): ");
-    scanf(" %c", &tempType);
-
-    printf("\nGib eine Uhrzeit ein (8-21): ");
-    scanf(" %d", &newAppointment.time);
-
-    printf("\nGib eine Dauer ein (1-%d): ", 22 - newAppointment.time);
-    scanf(" %d", &newAppointment.duration);
-
-    if ((newAppointment.time < 8 || newAppointment.time > 21)
-        || strchr("fbp", tempType) != NULL
-        || (newAppointment.duration < 1 || newAppointment.duration < 22 - newAppointment.time))
+    while (1)
     {
-        printf("Ungeultige Eingabe!");
-        return createAppointment();
+
+        printf("\nGib einen Typen ein: Freunde (f), Geschaeftlich (b), Pause (p): ");
+        scanf(" %c", &tempType);
+
+        if (strchr("fbp", tempType) == NULL)
+        {
+            printf("Ungeultige Eingabe!");
+            continue;
+        }
+        newAppointment.type = tempType;
+
+        printf("\nGib eine Uhrzeit ein (8-21): ");
+        scanf(" %d", &newAppointment.time);
+
+        if ((newAppointment.time < 8 || newAppointment.time > 21))
+        {
+            printf("Ungeultige Eingabe!");
+            continue;
+        }
+
+        printf("\nGib eine Dauer ein (1-%d): ", 22 - newAppointment.time);
+        scanf(" %d", &newAppointment.duration);
+
+        if (newAppointment.duration < 1 || newAppointment.duration > 22 - newAppointment.time)
+        {
+            printf("Ungeultige Eingabe!");
+            continue;
+        }
+        break;
     }
-    newAppointment.type = tempType;
 
     return newAppointment;
 }
@@ -60,41 +75,43 @@ void printAppointments(struct appointment* schedule, int length)
     {
         printf("\n%d: %s: %dUhr, Dauer %dh",
                i + 1,
-               getDateTypeAsString(schedule->type),
-               schedule->time,
-               schedule->duration
+               getDateTypeAsString(schedule[i].type),
+               schedule[i].time,
+               schedule[i].duration
         );
     }
 }
 
 void addAppointment(struct appointment* schedule, int* length)
 {
-    if(*length == 10) {
+    if (*length == 10)
+    {
         printf("Der Kalender ist voll!");
         return;
     }
 
     appointment newAppointment = createAppointment();
-    for (int i = *length - 1; i >= 0; --i)
+
+    for (int i = *length; i >= 0; --i)
     {
-        if(i == 0) {
+        if (!i || schedule[i - 1].time <= newAppointment.time)
+        {
             schedule[i] = newAppointment;
+            (*length)++;
             return;
         }
-        schedule[i + 1] = schedule[i];
-    }
 
+        schedule[i] = schedule[i - 1];
+    }
 }
 
 int main()
 {
-    char input;
-    dateType type;
     appointment appointments[10];
-    int appointmentCount;
+    int appointmentCount = 0;
+    char prozess;
 
-    while (1)
-    {
+    while(1) {
 
     }
 
