3ecebdc51095d9df2a2f493503b245078b96fbda
SwipSwup <david.veigel2002@gmail.com>
2023-11-06 22:56:18 +0100

    hidden test pupsi kakai

--- main.c
+++ main.c
@@ -8,6 +8,15 @@ enum dateType
     pause = 'p'
 } typedef dateType;
 
+enum action
+{
+    new = 'n',
+    delete = 'd',
+    list = 'l',
+    info = 'i',
+    exit = 'x'
+} typedef action;
+
 struct appointment
 {
     dateType type;
@@ -20,12 +29,93 @@ char* getDateTypeAsString(dateType type)
     switch (type)
     {
         case friends:
-            return "freunde";
+            return "Freunde";
         case business:
-            return "Geschäftlich";
+            return "Geschaeftlich";
         case pause:
             return "Pause";
     }
+
+    return "";
+}
+
+action getNextAction()
+{
+    char tmp;
+    printf("\nAuswahl: ");
+    scanf(" %c", &tmp);
+    return (action) tmp;
+}
+
+void printAppointments(struct appointment* schedule, int length)
+{
+    for (int i = 0; i < length; ++i)
+    {
+        printf("\n%d: %s: %d Uhr, Dauer %dh",
+               i + 1,
+               getDateTypeAsString(schedule[i].type),
+               schedule[i].time,
+               schedule[i].duration
+        );
+    }
+}
+
+void printInformation(struct appointment* schedule, int length)
+{
+    if (length == 0)
+    {
+        printf("\nDer Kalender hat keine Kollisionen!");
+    }
+
+    for (int i = 0; i < length; ++i)
+    {
+        if (i == length - 1)
+        {
+            printf("\nDer Kalender hat keine Kollisionen!");
+            break;
+        }
+
+        if (schedule[i].time + schedule[i].duration > schedule[i + 1].time)
+        {
+            printf("\nEntweder Termin %d oder Termin %d sollte abgesagt werden.", i + 1, i + 2);
+            break;
+        }
+    }
+
+    for (int i = 0; i < length - 1; ++i)
+    {
+        if (schedule[i].time + schedule[i].duration < schedule[i + 1].time)
+        {
+            printf("\nZwischen den Terminen %d und %d liegen %dh ungeplante Zeit.",
+                   i + 1,
+                   i + 2,
+                   schedule[i + 1].time - (schedule[i].time + schedule[i].duration)
+            );
+        }
+    }
+
+    int friendCount = 0, businessCount = 0, pauseCount = 0;
+    for (int i = 0; i < length; ++i)
+    {
+        switch (schedule[i].type)
+        {
+            case friends:
+                friendCount++;
+                break;
+            case business:
+                businessCount++;
+                break;
+            case pause:
+                pauseCount++;
+                break;
+        }
+    }
+
+    printf("\nSie haben %d Geschaeftstermin(e), %d Termin(e) mit Freunden und %d Pause(n) in Ihrem Kalender.",
+           businessCount,
+           friendCount,
+           pauseCount
+    );
 }
 
 appointment createAppointment()
@@ -41,26 +131,34 @@ appointment createAppointment()
 
         if (strchr("fbp", tempType) == NULL)
         {
-            printf("Ungeultige Eingabe!");
+            printf("\nUngueltige Eingabe!");
             continue;
         }
-        newAppointment.type = tempType;
+        newAppointment.type = (dateType) tempType;
+        break;
+    }
 
+    while (1)
+    {
         printf("\nGib eine Uhrzeit ein (8-21): ");
         scanf(" %d", &newAppointment.time);
 
         if ((newAppointment.time < 8 || newAppointment.time > 21))
         {
-            printf("Ungeultige Eingabe!");
+            printf("\nUngueltige Eingabe!");
             continue;
         }
+        break;
+    }
 
+    while (1)
+    {
         printf("\nGib eine Dauer ein (1-%d): ", 22 - newAppointment.time);
         scanf(" %d", &newAppointment.duration);
 
         if (newAppointment.duration < 1 || newAppointment.duration > 22 - newAppointment.time)
         {
-            printf("Ungeultige Eingabe!");
+            printf("\nUngueltige Eingabe!");
             continue;
         }
         break;
@@ -69,24 +167,11 @@ appointment createAppointment()
     return newAppointment;
 }
 
-void printAppointments(struct appointment* schedule, int length)
-{
-    for (int i = 0; i < length; ++i)
-    {
-        printf("\n%d: %s: %dUhr, Dauer %dh",
-               i + 1,
-               getDateTypeAsString(schedule[i].type),
-               schedule[i].time,
-               schedule[i].duration
-        );
-    }
-}
-
 void addAppointment(struct appointment* schedule, int* length)
 {
     if (*length == 10)
     {
-        printf("Der Kalender ist voll!");
+        printf("\nDer Kalender ist voll!");
         return;
     }
 
@@ -105,15 +190,69 @@ void addAppointment(struct appointment* schedule, int* length)
     }
 }
 
+void deleteAppointment(struct appointment* schedule, int* length)
+{
+    if (*length == 0)
+    {
+        printf("\nDer Kalender ist leer!");
+        return;
+    }
+
+    int index;
+    printAppointments(schedule, *length);
+    printf("\nWelchen dieser Termine moechten Sie loeschen? (1-%d): ", *length);
+    while (1)
+    {
+        scanf(" %d", &index);
+        if (index < 1 || index > *length)
+        {
+            printf("Ungueltige Eingabe!");
+            continue;
+        }
+        break;
+    }
+
+    printf("\nTermin %d wurde geloescht!", index);
+
+    for (int i = index - 1; i < *length - 1; ++i)
+    {
+        schedule[i] = schedule[i + 1];
+    }
+    (*length)--;
+}
+
 int main()
 {
-    appointment appointments[10];
-    int appointmentCount = 0;
-    char prozess;
+    appointment schedule[10];
+    int scheduleCount = 0;
+    action nextAction;
 
-    while(1) {
+    do
+    {
+        printf("\nNeuen Termin (n), Termin loeschen (d), Termine auflisten (l), Informationen ausgeben (i), Programm beenden (x)");
 
-    }
+        nextAction = getNextAction();
+
+        switch (nextAction)
+        {
+            case new:
+                addAppointment(schedule, &scheduleCount);
+                break;
+            case delete:
+                deleteAppointment(schedule, &scheduleCount);
+                break;
+            case list:
+                printAppointments(schedule, scheduleCount);
+                break;
+            case info:
+                printInformation(schedule, scheduleCount);
+                break;
+            case exit:
+                break;
+            default:
+                printf("\nUngueltige Eingabe!");
+        }
+    } while (nextAction != exit);
 
     return 0;
 }
