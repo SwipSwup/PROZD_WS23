ec98b170404d4c26a9ca39c49be3e09c39b290c1
SwipSwup <david.veigel2002@gmail.com>
2023-11-08 22:37:10 +0100

    FinishUE18

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main() {
-    printf("Hello, World!\n");
+#define MAX_PALINDROME_LENGTH 40
+
+int main()
+{
+    char nextChar;
+    int palindromeLength = 0;
+    char palindrome[MAX_PALINDROME_LENGTH];
+
+    printf(": ");
+    while (1)
+    {
+        scanf("%c", &nextChar);
+
+        if (nextChar == '\n' || palindromeLength == MAX_PALINDROME_LENGTH)
+        {
+            break;
+        }
+
+        if (!isalpha(nextChar))
+        {
+            continue;
+        }
+
+        palindrome[palindromeLength] = (char) tolower(nextChar);
+        palindromeLength++;
+    }
+
+    for (int i = 0; i < palindromeLength / 2; ++i)
+    {
+        if(palindrome[i] != palindrome[palindromeLength - 1 - i]) {
+            printf("false");
+            return 0;
+        }
+    }
+
+    printf("true");
     return 0;
 }
