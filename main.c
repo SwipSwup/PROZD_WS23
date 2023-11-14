b302f2499c49099e0e540b1f1356071155a81b06
SwipSwup <david.veigel2002@gmail.com>
2023-11-14 11:31:42 +0100

    argh

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <malloc.h>
 
-int main() {
+#define BOARD_SIZE 10
+
+char** initializeBoard()
+{
+    char** board = (char**) malloc(sizeof(char*) * BOARD_SIZE);
+    for (int x = 0; x < BOARD_SIZE; ++x)
+    {
+        board[x] = (char *) malloc(sizeof (char) * BOARD_SIZE);
+        for (int y = 0; y < BOARD_SIZE; ++y)
+        {
+            board[x][y] = '.';
+        }
+    }
+    return board;
+}
+
+int main()
+{
+
+
     printf("Hello, World!\n");
     return 0;
 }
