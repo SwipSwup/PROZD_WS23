4504de2d4846b6a7362b001b9ab813a7b9795c8a
SwipSwup <david.veigel2002@gmail.com>
2023-12-04 12:24:43 +0100

    asd

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+enum genre_type
+{
+    HORROR,
+    ADVENTURE,
+    ROMANCE,
+    NON_FICTION
+} typedef genre_type;
+
+struct book
+{
+    char* title;
+    genre_type genre;
+    short year;
+    int amount;
+} typedef book;
+
+int main()
+{
+    dkjjijsfjs
     return 0;
 }
